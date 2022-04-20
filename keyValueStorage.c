f653f16be9f0dcc2ebb7c641b3156e3397e673c0
Finn <finnjensen@gmx.de>
2022-04-20 14:10:16 +0200

    inital commit

---
+++ keyValueStorage.c
@@ -0,0 +1,9 @@
+//
+// Created by finnj on 19.04.2022.
+//
+
+#include "keyValueStorage.h"
+
+//TODO: Implement Key:Value Pair Storage structure (Can be an Array or Struct)
+
+//TODO: Add Persistence to Storage
---
+++ subRoutines.c
@@ -0,0 +1,11 @@
+//
+// Created by finnj on 19.04.2022.
+//
+
+#include "subRoutines.h"
+
+//TODO: Implement int put(char * key, char * value). If already exists -> Overwrite.
+
+//TODO: Implement int get(char * key, char * res). If not found: return -1
+
+//TODO: Implement int del(char * key).
